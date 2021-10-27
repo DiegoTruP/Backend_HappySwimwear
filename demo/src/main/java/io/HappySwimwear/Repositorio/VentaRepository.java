f349a699f7f47cc5ad515257dde5af35f8c54a94
Ppabl0 <89792989+Ppabl0@users.noreply.github.com>
2021-10-27 15:41:25 -0500

    Delete demo directory

--- demo/src/main/java/io/HappySwimwear/Repositorio/VentaRepository.java
+++
@@ -1,8 +0,0 @@
-package io.HappySwimwear.Repositorio;
-
-import io.HappySwimwear.Modelo.Venta;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VentaRepository extends CrudRepository<Venta, Integer> {
-    Venta findById(int id);
-}
