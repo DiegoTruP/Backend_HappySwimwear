0b7ef9e2c553f4a8c9f39005c0d1419f8cca76f9
Ppabl0 <89792989+Ppabl0@users.noreply.github.com>
2021-10-27 11:54:54 -0500

    agregar jwt

---
+++ demo/src/main/java/io/HappySwimwear/Repositorio/UserRepository.java
@@ -0,0 +1,12 @@
+package io.HappySwimwear.Repositorio;
+
+import io.HappySwimwear.Modelo.User;
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    User findById(int id);
+    User findByCorreo(String correo);
+}
