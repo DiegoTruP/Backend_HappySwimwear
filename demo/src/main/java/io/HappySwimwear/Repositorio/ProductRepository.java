aa67fcff966882137338342e2586ed113e57b11a
Ppabl0 <89792989+Ppabl0@users.noreply.github.com>
2021-10-27 15:33:53 -0500

    delete

--- demo/src/main/java/io/HappySwimwear/Repositorio/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package io.HappySwimwear.Repositorio;
-
-import io.HappySwimwear.Modelo.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-    Product findById(int id);
-}
