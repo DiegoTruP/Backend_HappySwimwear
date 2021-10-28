96168f3040e2ccdd6fa5dfff50c63c4e3338d8a4
Ppabl0 <89792989+Ppabl0@users.noreply.github.com>
2021-10-28 13:19:16 -0500

    habilitar crossOrigin

--- demo/src/main/java/io/HappySwimwear/Controlador/ProductController.java
+++ demo/src/main/java/io/HappySwimwear/Controlador/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @Controller
 @RequestMapping(path="/productos")
 public class ProductController {
